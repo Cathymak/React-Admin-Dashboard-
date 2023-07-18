a29cec8a468f53c4323ad4a54d23b3d8b93a8dbe
Katlego Cathy Makiwa <katlego191@gmail.com>
2023-07-19 00:36:24 +0200

    sidebar.css & sidebar.js

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,36 +1,28 @@
 import React from 'react';
 import './Sidebar.css';
 import Logo from '../../imgs/logo.png';
-import  UilEstate from '@iconscout/react-unicons/icons/uil-estate'
-
+import UilEstate from '@iconscout/react-unicons/icons/uil-estate';
 
 const sidebar = () => {
-  return(
-  <div className="sidebar">
-    <div className="Logo">
-      <img src={Logo} alt="Logo" />
-      <span>
+  return (
+    <div className="sidebar">
+      <div className="Logo">
+        <img src={Logo} alt="Logo" />
+        <span>
           sh<span>o</span>ps
         </span>
-         </div>
+      </div>
 
-<div className='menu'>
-
-<div className='menuItem'>
-
-<div> <UilEstate/></div>
-<span></span>
-
-</div>
-
-</div>
-        <span> Dashboard</span>
+      <div className="menu">
+        <div className="menuItem">
+          <div>
+            {' '}
+            <UilEstate />
+          </div>
+          <span>Dashboard</span>
+        </div>
+      </div>
     </div>
-
-
-
-
-
-  )
+  );
 };
 export default sidebar;

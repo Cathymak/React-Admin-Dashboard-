6a3806c8a52499ebb782f6f1506551007b055933
Katlego Cathy Makiwa <katlego191@gmail.com>
2023-07-24 13:06:06 +0200

    Creation of new folder and files
    MainDash.css & Maindash.js as well as Cards.css & Cards.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import MainDash from './components/MainDash/MainDash';
 import Sidebar from './components/sidebar/Sidebar';
+
 
 function App() {
   return (
     <div className="App">
       <div className="AppGlass">
       <Sidebar/>
-      
+      <MainDash/>
       </div>
     </div>
   );
---
+++ src/components/MainDash/MainDash.js
@@ -0,0 +1,17 @@
+import { ClassNames } from "@emotion/react";
+import React from "react";
+
+const MainDash =()=> {
+
+    return (
+   <div className="MainDash">
+   
+   <h1>Dashboard</h1>
+   <card/>
+   </div>
+       
+    )
+
+}
+
+export default MainDash;

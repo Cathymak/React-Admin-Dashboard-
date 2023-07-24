17fc4eef4afbcd4a9b5d06d5748f3ffc45b42c46
Katlego Cathy Makiwa <katlego191@gmail.com>
2023-07-24 13:54:32 +0200

    Creation of reusable component
    Creation of reusable component for the Dashboard cards by adding an array of data within the Data.js file as well as the creation of 1 new folder and 2 new files Card.js and Card.css

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,2 +1,49 @@
 import React from "react";
+import "./Cards.css"
+import { Card } from "@mui/material";
 
+
+const Cards=()=>{
+
+return(
+
+    <div className="Cards"> 
+
+    
+
+// This is how to call the array of data from the data.js file specifically for the data that will fill the Dashboard cards. 
+    {CardsData.map((card, id)=> {
+
+        return(
+
+
+            <div className="parentContainer">
+
+
+
+            <Card
+
+            title={card.title}
+            color={card.color}
+            barValue={card.barValue}
+            iconName={card.iconName}
+            value={card.value}
+            png={card.png}
+            series={card.series}
+            />
+
+
+            </div>
+        )
+        })}
+    
+
+    
+    
+    
+    </div>
+)
+}
+
+
+export default Cards;
